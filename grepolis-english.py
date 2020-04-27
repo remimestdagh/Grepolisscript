b7b802d02f494262d679f499a98fbc093470e936
Remi Mestdagh <47556303+remimestdagh@users.noreply.github.com>
2020-04-27 19:08:08 +0200

    Update grepolis-english.py

--- grepolis-english.py
+++ grepolis-english.py
@@ -12,8 +12,8 @@ from selenium.webdriver.chrome.options import Options
 ###############
 url = "https://nl.grepolis.com/"
 print('################ GREPOLIS  BOT')
-username = "HenkHamster"#input('Username: ')
-password = "apenkool8"#input('Password: ')
+username = ""#input('Username: ')
+password = ""#input('Password: ')
 selected_town = "Stad van HenkHamster" #input("City name( 0 to don't select a main city ): ")
 towns_number = int(input('Number of cities: '))
 world_number = int(input('World number: '))
